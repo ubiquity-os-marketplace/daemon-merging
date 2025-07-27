5232d8d1fbe51ba78f2b1a7add1a39a8f9108e5f
gentlementlegen <fernand.veyrier@epitech.eu>
2025-07-27 21:36:07 +0200

    refactor: simplify plugin context handling by removing adapters

--- src/worker.ts
+++ src/worker.ts
@@ -2,16 +2,13 @@ import { createPlugin } from "@ubiquity-os/plugin-sdk";
 import { Manifest } from "@ubiquity-os/plugin-sdk/dist/manifest";
 import { LOG_LEVEL } from "@ubiquity-os/ubiquity-os-logger";
 import manifest from "../manifest.json";
-import { createAdapters } from "./adapters/index";
 import { plugin } from "./plugin";
-import { Context, Env, envSchema, SupportedEvents } from "./types/index";
+import { Env, envSchema, SupportedEvents } from "./types/index";
 import { PluginSettings, pluginSettingsSchema } from "./types/plugin-input";
 
 const app = createPlugin<PluginSettings, Env, null, SupportedEvents>(
   async (context) => {
-    const adapters = await createAdapters();
-    const contextWithAdapters = { ...context, adapters } as Context;
-    return plugin(contextWithAdapters);
+    return plugin(context);
   },
   manifest as Manifest,
   {
