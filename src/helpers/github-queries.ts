a916eb1b2820622547cf26038d2e238be4861f24
gentlementlegen <fernand.veyrier@epitech.eu>
2025-07-28 13:13:16 +0200

    chore: add GraphQL query for fetching linked pull requests

---
+++ src/helpers/github-queries.ts
@@ -0,0 +1,44 @@
+import type { Issue, PullRequestConnection } from "@octokit/graphql-schema";
+
+export const QUERY_LINKED_PULL_REQUESTS = /* GraphQL */ `
+  query collectLinkedPullRequests($owner: String!, $repo: String!, $issueNumber: Int!, $cursor: String) {
+    repository(owner: $owner, name: $repo) {
+      issue(number: $issueNumber) {
+        id
+        closedByPullRequestsReferences(first: 10, includeClosedPrs: false, after: $cursor) {
+          edges {
+            node {
+              id
+              title
+              number
+              url
+              state
+              isDraft
+              author {
+                login
+              }
+              repository {
+                owner {
+                  login
+                }
+                name
+              }
+            }
+          }
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+        }
+      }
+    }
+  }
+`;
+
+export interface LinkedPullRequestsResponse {
+  repository?: {
+    issue?: Pick<Issue, "id"> & {
+      closedByPullRequestsReferences?: PullRequestConnection;
+    };
+  };
+}
