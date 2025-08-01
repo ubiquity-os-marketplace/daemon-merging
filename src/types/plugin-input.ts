b2405f8ee04e8b3b559cda965e6dde30b06c3eb3
gentlementlegen <fernand.veyrier@epitech.eu>
2025-08-01 14:17:13 +0200

    chore: remove unused GraphQL query and dependencies

--- src/types/plugin-input.ts
+++ src/types/plugin-input.ts
@@ -76,4 +76,3 @@ export const pluginSettingsSchema = T.Object({
 });
 
 export type PluginSettings = StaticDecode<typeof pluginSettingsSchema>;
-export type ExcludedRepos = StaticDecode<typeof excludedReposSchema>;
