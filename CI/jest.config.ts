c35ca04e4dbcca6cad6967cef092c84ecef8543f
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2025-11-14 15:48:39 +0000

    chore: update Jest configuration for CI and correct output paths

--- CI/jest.config.ts
+++ CI/jest.config.ts
@@ -2,13 +2,14 @@ import type { Config } from "jest";
 
 const cfg: Config = {
   testEnvironment: "node",
-  roots: ["<rootDir>/tests"],
-  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
+  rootDir: "..",
+  roots: ["<rootDir>/CI/tests"],
+  setupFilesAfterEnv: ["<rootDir>/CI/tests/setup.ts"],
   coveragePathIgnorePatterns: ["node_modules", "mocks"],
   collectCoverage: true,
   coverageReporters: ["json", "lcov", "text", "clover", "json-summary"],
-  reporters: ["default", "jest-junit", ["jest-md-dashboard", { output: "CI/test-dashboard.md" }]],
-  coverageDirectory: "coverage",
+  reporters: ["default", "jest-junit", ["jest-md-dashboard", { outputPath: "CI/test-dashboard.md" }]],
+  coverageDirectory: "CI/coverage",
   transformIgnorePatterns: [],
   transform: {
     "^.+\\.[jt]s$": ["@swc/jest", {}],
