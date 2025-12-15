5da1020d2c4da37617af5e2838d6e001009cf463
0x4007 <gpg@pavlovcik.com>
2025-12-15 15:09:41 +0900

    chore: preserve latest changes

--- src/types/env.ts
+++ src/types/env.ts
@@ -3,7 +3,7 @@ import { StaticDecode } from "@sinclair/typebox";
 import "dotenv/config";
 
 export const envSchema = T.Object({
-  workflowName: T.String({ default: "compute.yml" }),
+  workflowName: T.String({ default: "action.yml" }),
 });
 
 export type Env = StaticDecode<typeof envSchema>;
